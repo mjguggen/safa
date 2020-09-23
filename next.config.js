1a2ef49d2574fd9493c7d2d181f63f6abe114885
Mike Guggenbuehl <guggenz@Mikes-MacBook-Pro.local>
2020-09-23 11:50:10 -0500

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 const withImages = require('next-images')
+const withPlugins = require('next-compose-plugins');
 
-exports.target = 'severless'
+
+
+
+// exports.target = 'severless'
 
 module.exports = withImages()
+
+module.exports = withPlugins(
+    [
+        withImages()
+    ],
+    {
+        target: 'serverless'
+    }
+)
